f6bf91e491e2f0520d5dd97d83b94c149de630c7
davood.hanifi <davood.hanifi@gmail.com>
2022-01-17 00:02:13 +0330

    Created Staircase.

---
+++ Interview Preparation Kit/Warm-up Challenges/Staircase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void Staircase(int n)
+    {
+        for (int i = 1; i<= n; i++)
+        {
+            for (int j = i; j<n; j++)
+                Console.Write(" ");
+
+            for (int k = i; k>0; k--)
+                Console.Write("#");
+
+            Console.WriteLine();
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.Staircase(n);
+    }
+}
