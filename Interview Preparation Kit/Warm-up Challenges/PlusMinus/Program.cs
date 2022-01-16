629f83deee935f04764c805b4bbf5ed32cd4e2b3
davood.hanifi <davood.hanifi@gmail.com>
2022-01-16 23:42:48 +0330

    Created plus minus.

---
+++ Interview Preparation Kit/Warm-up Challenges/PlusMinus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void PlusMinus(List<int> arr)
+    {
+        var count = (double)arr.Count;
+        System.Console.WriteLine(arr.Count(x => x > 0) / count);
+        System.Console.WriteLine(arr.Count(x => x < 0) / count);
+        System.Console.WriteLine(arr.Count(x => x == 0) / count);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.PlusMinus(arr);
+    }
+}
