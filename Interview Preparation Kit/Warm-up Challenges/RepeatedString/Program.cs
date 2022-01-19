d00393ddb0d0433876ec737fb8995740dd976b27
davood.hanifi <davood.hanifi@gmail.com>
2022-01-20 00:43:16 +0330

    Added repeated string.

---
+++ Interview Preparation Kit/Warm-up Challenges/RepeatedString/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long RepeatedString(string s, long n)
+    {
+        long result = 0;
+        var length = s.Length;
+        var wordCount = n / length;
+        var aCountInS = s.Count(x => x == 'a');
+        var rest = (int)(n % length);
+        result = wordCount * aCountInS;
+        var lastPart = s.Substring(0, rest);
+
+        return result + lastPart.Count(x => x == 'a');
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+        long result = Result.RepeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
