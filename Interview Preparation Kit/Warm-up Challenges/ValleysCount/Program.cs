662f8f22d40961c1c8bc34ee0d1f4da9336ffa54
davood.hanifi <davood.hanifi@gmail.com>
2022-01-21 11:23:54 +0330

    Added valleys count.

---
+++ Interview Preparation Kit/Warm-up Challenges/ValleysCount/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int CountingValleys(int steps, string path)
+    {
+        var currentLevel = 0;
+        var valleysCount = 0;
+
+        foreach (var step in path)
+        {
+            if (step == 'U')
+            {
+                if (currentLevel == -1)
+                    valleysCount++;
+
+                currentLevel++;
+            }
+            else
+            {
+                currentLevel--;
+            }
+        }
+
+        return valleysCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.CountingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
