1e8b49a487c4a9880768d234885b22ce0fd1204b
davood.hanifi <davood.hanifi@gmail.com>
2022-01-26 22:52:32 +0330

    Added array rotator.

---
+++ Interview Preparation Kit/Arrays/ArrayRotator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+    public static int[] RotLeft(int[] array, int rotNumber)
+    {
+        var arraySize = array.Length;
+        var result = new int[arraySize];
+
+        var k = 0;
+        for (var i = arraySize - rotNumber; i <= arraySize -1; i++)
+            result[i] = array[k++];
+
+        var j = 0;
+        for (var i = 0; i < arraySize - rotNumber; i++)
+            result[j++] = array[k++];
+
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        var array = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+
+        var result = Result.RotLeft(array, d);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
