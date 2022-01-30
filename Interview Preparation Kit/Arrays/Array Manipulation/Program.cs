47684a492c34f0ca9129ed2cce8f135527be3ce2
davood.hanifi <davood.hanifi@gmail.com>
2022-01-30 23:57:19 +0330

    Added Array Manipulation

---
+++ Interview Preparation Kit/Arrays/Array Manipulation/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static long ArrayManipulation(int n, int[][] queries)
+    {
+        var array = new long[n];
+        long max = 0;
+        long sum = 0;
+
+        for (var i = 0; i < queries.Length; i++) 
+        {
+            var a = queries[i][0] - 1;
+            var b = queries[i][1] - 1;
+            var k = queries[i][2];
+
+            array[a] += k;
+            if (b + 1 < n)
+            {
+                array[b + 1] -= k;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += array[i];
+            max = Math.Max(max, sum);
+        }
+
+        return max;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        //string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        //int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        //int m = Convert.ToInt32(firstMultipleInput[1]);
+        var queries = new int[3][];
+
+        //for (int i = 0; i < m; i++)
+        //    queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        queries[0] = new int[]{ 2,3,603};
+        queries[1] = new int[]{ 1,1,286};
+        queries[2] = new int[]{ 4,4,882};
+        var result = Result.ArrayManipulation(4, queries);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
