7488f9f615f5513fd9a25b2752aa8e2600492030
davood.hanifi <davood.hanifi@gmail.com>
2022-02-23 12:10:02 +0330

    Added two strings common substring.

---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/CommonSubstring/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static string CommonSubstring(string s1, string s2)
+    {
+        var result = "NO";
+        s1.
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        var q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (var qItr = 0; qItr < q; qItr++)
+        {
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+            var result = Result.CommonSubstring(s1, s2);
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/TwoStrings/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static string TwoStrings(string first, string second)
+    {
+        foreach (var characterGroup in first.GroupBy(item => item))
+        {
+            if (second.Contains(characterGroup.Key))
+                return "YES";
+        }
+
+        return "NO";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        var q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            var first = Console.ReadLine();
+            var second = Console.ReadLine();
+            var result = Result.TwoStrings(first, second);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
