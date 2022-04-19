527b8f29faaedf837b35cc103c26628a4b4e050f
davood.hanifi <davood.hanifi@gmail.com>
2022-04-19 22:17:13 +0430

    Added AlternatingCharacters.

---
+++ Interview Preparation Kit/String Manipulation/AlternatingCharacters/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'alternatingCharacters' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int AlternatingCharacters(string text)
+    {
+        var result = 0;
+        var previousCharacter = default(char);
+        foreach (char character in text)
+        {
+            if (previousCharacter != default(char) && previousCharacter == character)
+                result++;
+
+            previousCharacter = character;
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        var q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            var s = Console.ReadLine();
+
+            var result = Result.AlternatingCharacters(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
