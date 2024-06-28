988e809c30b51c500b9797f5e3650306dd98133d
Davood Hanifi <davood.hanifi@gmail.com>
2024-06-29 00:39:52 +0200

    Added TwoSum in Arrays.

---
+++ Interview Preparation Kit/Arrays/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> numDic = []; 
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var result = target - nums[i];
+
+        if (numDic.TryGetValue(result, out int value))
+        {
+            return [i, value];
+        }
+
+        numDic[nums[i]] = i;
+    }
+
+    throw new ArgumentException($"Not match found for the given target {target}");
+}
