fb740ac7b98246a2579d6b8d5bd9b4832de4941f
Davood Hanifi <davood.hanifi@gmail.com>
2024-06-29 23:37:34 +0200

    Added RemoveDuplicates in array.

---
+++ Interview Preparation Kit/Arrays/RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -0,0 +1,19 @@
+﻿static int RemoveDuplicates(int[] nums)
+{
+    if (nums.Length == 0)
+    {
+        return 0;
+    }
+
+    var currentIndex = 1;
+    for (var i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            nums[currentIndex] = nums[i];
+            currentIndex++;
+        }
+    }
+
+    return currentIndex;
+}
