5675c7f4d3ef7a67e603969131143cec83a2104a
Davood Hanifi <davood.hanifi@gmail.com>
2024-06-29 23:57:12 +0200

    Added RemoveElement in array.

---
+++ Interview Preparation Kit/Arrays/RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,19 @@
+﻿static int RemoveElement(int[] nums, int val)
+{
+    if (nums.Length == 0)
+    {
+        return 0;
+    }
+
+    var currentIndex = 0;
+    for (var i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != val)
+        {
+            nums[currentIndex] = nums[i];
+            currentIndex++;
+        }
+    }
+
+    return currentIndex;
+}
