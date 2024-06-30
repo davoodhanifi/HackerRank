8c1c4a5b937d39d2815cc723d5fa97fc2871679f
Davood Hanifi <davood.hanifi@gmail.com>
2024-06-30 17:13:15 +0200

    Added Subsets in array

---
+++ Interview Preparation Kit/Arrays/Subsets/Subsets/Program.cs
@@ -0,0 +1,22 @@
+﻿static IList<IList<int>> Subsets(int[] nums)
+{
+    IList<IList<int>> result = [[]];
+
+    result[0] = [];
+    foreach (int num in nums)
+    {
+        int currentCount = result.Count;
+        for (int i = 0; i < currentCount; i++)
+        {
+            List<int> newSubset = new(result[i])
+            {
+                num
+            };
+            result.Add(newSubset);
+        }
+    }
+
+    return result;
+}
+
+Subsets([0,1,2]);
